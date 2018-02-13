5b55b11f380942913ac93f60e0f0a1597471c45e
FatherLuis <fatherluis98@gmail.com>
2018-02-12 19:44:27 -0600

    I'' likely consider using the build in Linked List since they provide a way to start at a certain Link rather than while-looping to get a Link

--- src/vargas_tamayo_cis4340_proj1/Vargas_tamayo_CIS4340_Proj1.java
+++ src/vargas_tamayo_cis4340_proj1/Vargas_tamayo_CIS4340_Proj1.java
@@ -8,6 +8,15 @@ public class Vargas_tamayo_CIS4340_Proj1
     public static void main(String[] args) 
     {
      
+        MergeSort MS = new MergeSort(10);
+        
+        
     }
+
+
+
+
+
+
     
 }
