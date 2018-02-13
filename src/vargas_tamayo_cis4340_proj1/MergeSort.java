2e661c9ab16ffe2d30d009476b7fd5e241923be0
FatherLuis <fatherluis98@gmail.com>
2018-02-12 18:19:36 -0600

    Created two classes

---
+++ src/vargas_tamayo_cis4340_proj1/MergeSort.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vargas_tamayo_cis4340_proj1;
+
+import java.util.LinkedList;
+
+/**
+ *
+ * @author fathe
+ */
+public class MergeSort 
+{
+    private LinkedList LinkedChain;
+    
+    public MergeSort()
+    {
+        LinkedChain = new LinkedList();
+    
+    }
+    
+    private void setUp()
+    {
+        
+        
+        this.LinkedChain.add(5);
+    
+    
+    
+    }
+    
+    
+    
+    
+    
+}
---
+++ src/vargas_tamayo_cis4340_proj1/Vargas_tamayo_CIS4340_Proj1.java
@@ -0,0 +1,13 @@
+
+package vargas_tamayo_cis4340_proj1;
+
+
+public class Vargas_tamayo_CIS4340_Proj1 
+{
+
+    public static void main(String[] args) 
+    {
+     
+    }
+    
+}
